c16121a20e6471c1fa49f67f73d14082298f7b03
Rafael N <rfaaelnc@gmail.com>
2022-01-26 22:22:07 -0300

    add useState in login and page chat

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,7 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
 import appConfig from '../config.json';
-
-const GlobalStyle = ()=> {
-  return (
-    <style global jsx>{`
-        * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-
-    `}</style>
-  )
-};
-
+import React from 'react';
+import { useRouter } from 'next/router';
 
 const Title = (props) => {
   console.log(props);
@@ -50,11 +21,13 @@ const Title = (props) => {
 };
 
 export default function PaginaInicial() {
-  const username = 'rfaaelnc';
+  // const username = 'rfaaelnc';
+  const [username,setUsername] = React.useState('peas');
+  const roteamento = useRouter();
 
   return (
     <>
-      <GlobalStyle />
+      {/* <GlobalStyle /> */}
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -81,6 +54,13 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={function(event) {
+              event.preventDefault();
+              // console.log(123);
+
+              roteamento.push('/chat')
+
+            }}
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -91,7 +71,31 @@ export default function PaginaInicial() {
               {appConfig.name}
             </Text>
 
+            {/* <input
+              type="text"
+              value={username}
+              onChange={function handler(event) {
+                console.log('usuario digitou', event.target.value);
+
+                const valueInput = event.target.value;
+
+                setUsername(valueInput);
+                
+              }}
+            /> */}
+
             <TextField
+            
+              value={username}
+              onChange={function handler(event) {
+                console.log('usuario digitou', event.target.value);
+
+                const valueInput = event.target.value;
+
+                setUsername(valueInput);
+                
+              }}
+
               fullWidth
               textFieldColors={{
                 neutral: {
@@ -102,6 +106,7 @@ export default function PaginaInicial() {
                 },
               }}
             />
+
             <Button
               type='submit'
               label='Entrar'
