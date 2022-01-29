6ca69629ba7c1544562d54ca33accbb5f6c68160
Rafael N <rfaaelnc@gmail.com>
2022-01-29 18:43:42 -0300

    delete config

---
+++ config.js
@@ -0,0 +1,7 @@
+let config = {
+  SUPABASE_PUBLIC_KEY:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzM3MzQyNiwiZXhwIjoxOTU4OTQ5NDI2fQ.4rOPCyfNXksJ80oIMZqVwi8_t-jT00wk4ZhEwas-Mi4",
+  SUPABASE_URL: "https://qardaowlnlgxaiuffzxo.supabase.co",
+};
+
+export default config;
